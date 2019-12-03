cb4d1ad67364feb3bc815cb76a3a9be5d533a88f
Joaquim Lopes <joaquim@autoconf.com.br>
2019-12-03 14:06:50 -0300

    criação de eventos

--- resources/views/veiculo/create.blade.php
+++ resources/views/veiculo/create.blade.php
@@ -1,4 +1,12 @@
 @extends('layout.template')
 @section('body')
     @include('veiculo.partials._form') 
-@endsection   
+    
+@endsection 
+@push('scripts')
+    <script>
+        $(document).ready(function(){
+            $('[name=placa]').mask('AAA-9S99');
+        });
+    </script>
+@endpush  
--- resources/views/veiculo/partials/_form.blade.php
+++ resources/views/veiculo/partials/_form.blade.php
@@ -3,16 +3,16 @@
     @csrf
     <div>
         <label for="_placa">Placa</label>
-        <input type="text" name="placa" id="_placa">
+        <input type="text" name="placa" id="_placa" value="{{old('placa', '')}}">
         @if($errors->has('placa'))
         <div class="invalid-feedback"> {{  $errors->first('placa') }}</div>
         @endif
     </div>
     <div>
         <label for="_chassi">Chassi</label>
-        <input type="text" name="chassi" id="_chassi">
+        <input type="text" name="chassi" id="_chassi" value="{{old('chassi', '')}}">
         @if($errors->has('chassi'))
-        <div class="invalid-feedback"> {{  $errors->first('chassi') }}</div>
+            <div class="invalid-feedback"> {{  $errors->first('chassi') }}</div>
         @endif
     </div>
     <div>
@@ -20,7 +20,7 @@
         <select name="marca" id="_marca">
             <option value="">--Seleciona--</option>
             @foreach ($marcas as $marca)
-                <option value="{{$marca->id}}">{{$marca->nome}}</option>
+                <option value="{{$marca->id}}" @if(old('marca', '' ) == $marca->id ) selected="" @endif>{{$marca->nome}}</option>
             @endforeach
         </select>
         @if($errors->has('marca'))
@@ -32,7 +32,7 @@
         <select name="modelo" id="_modelo">
             <option value="_modelo">--Seleciona--</option>
             @foreach ($modelos as $modelo)
-                <option value="{{$modelo->id}}">{{$modelo->nome}}</option>
+                <option value="{{$modelo->id}}" @if(old('modelo', '' ) == $modelo->id ) selected="" @endif>{{$modelo->nome}}</option>
             @endforeach
         </select>
         @if($errors->has('modelo'))
@@ -41,16 +41,16 @@
     </div>
     <div>
         <label for="_anomodelo">Ano Modelo</label>
-        <input type="text" name="anomodelo" id="_anomodelo">
+        <input type="text" name="anomodelo" id="_anomodelo" value="{{old('anomodelo', '')}}">
         @if($errors->has('anomodelo'))
-        <div class="invalid-feedback"> {{  $errors->first('anomodelo') }}</div>
+            <div class="invalid-feedback"> {{  $errors->first('anomodelo') }}</div>
         @endif
     </div>
     <div>
         <label for="_anofabricacao">Ano Fabricação</label>
-        <input type="text" name="anofabricacao" id="_anofabricacao">
+        <input type="text" name="anofabricacao" id="_anofabricacao" value="{{old('anofabricacao', '')}}">
         @if($errors->has('anofabricacao'))
-        <div class="invalid-feedback"> {{  $errors->first('anofabricacao') }}</div>
+            <div class="invalid-feedback"> {{  $errors->first('anofabricacao') }}</div>
         @endif
     </div>
     <div>
